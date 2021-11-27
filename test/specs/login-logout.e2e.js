9961e0f6cecf07b2c4ed12d939fdc9179b2eda42
Petra Juris <petra.gardavsk@gmail.com>
2021-11-27 18:52:52 +0100

    Fix login logout tests

--- test/specs/login-logout.e2e.js
+++ test/specs/login-logout.e2e.js
@@ -18,8 +18,25 @@ describe('Three foxes Login Page', () => {
         browser.pause(5000);
         const userProfileButton = $('#user_info_acc')
         expect(userProfileButton.getText()).toEqual('Ester');
-
+    });
+    it('logout', () => {
+        browser.reloadSession();
+        browser.url('/en/login?back=my-account');
+        //email
+        const emailField = $('#email');
+        expect(emailField).toBeDisplayed();
+        expect(emailField).toBeEnabled();
+        //password
+        const passwordField = $('#passwd');
+        expect(passwordField).toBeDisplayed();
+        expect(passwordField).toBeEnabled();
+        const button = $('#SubmitLogin');
+        expect(button.getText()).toEqual('Sign in');
+        emailField.setValue('avybenjmincox@gmail.com');
+        passwordField.setValue('jsmenejlepsi5');
+        button.click();
         browser.pause(5000);
+        
         $('#user_info_acc').click();
         browser.pause(2000);
         $('=Logout').click();
